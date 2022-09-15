e13e5da4391d4582b8a0efb42a16f28c50857128
galgue <galgue11@gmail.com>
2022-09-15 15:45:49 +0300

    add pokemon table manager

--- client/src/pages/pokemon-list/PokemonList.tsx
+++ client/src/pages/pokemon-list/PokemonList.tsx
@@ -1,19 +1,21 @@
-import { createResource, createSignal, For } from 'solid-js';
-import { fetchPokemonPage } from '../../api/fetchPokemonPage';
+import { For } from 'solid-js';
 import { DebouncedInput } from '../../components/DebouncedInput';
 import { FaSolidArrowLeft, FaSolidArrowRight } from 'solid-icons/fa';
 import { Pokemon } from './pokemon';
+import { createPokemonTable } from './createPokemonTable';
 
 const listLayout = 'w-5/6 flex flex-wrap flex-row items-center justify-center';
 const listContainer = 'flex flex-row justify-center items-center h-full';
 
 export const PokemonList = () => {
-    const [input, setInput] = createSignal('');
-    const [pageNumber, setPageNumber] = createSignal(0);
-
-    const [page] = createResource(pageNumber, (pageNumberCurrent) =>
-        fetchPokemonPage(pageNumberCurrent)
-    );
+    const {
+        nextPage,
+        page,
+        previousPage,
+        setInput,
+        canNextPage,
+        canPreviousPage,
+    } = createPokemonTable();
 
     return (
         <div class="flex flex-col justify-center items-center h-full pt-10">
@@ -26,18 +28,12 @@ export const PokemonList = () => {
                 onChange={(value) => setInput(value)}
             />
             <div class={listContainer}>
-                {/* <ul>
-                    <For each={page()?.pokemons}>
-                        {(pokemon, i) => <li>{pokemon.name}</li>}
-                    </For>
-                </ul> */}
                 <div class="flex justify-center items-center">
                     <FaSolidArrowLeft
-                        class="text-poke-light-blue text-4xl cursor-pointer"
-                        // class={`w-24 cursor-pointer ${
-                        //     !pagination.canPrevPage ? 'invisible' : ''
-                        // }`}
-                        onClick={() => setPageNumber(pageNumber() - 1)}
+                        class={`text-poke-light-blue text-4xl w-24 cursor-pointer ${
+                            !canPreviousPage() ? 'invisible' : ''
+                        }`}
+                        onClick={() => previousPage()}
                     />
                 </div>
                 <div class={listLayout}>
@@ -53,11 +49,10 @@ export const PokemonList = () => {
                 </div>
                 <div class="flex justify-center items-center">
                     <FaSolidArrowRight
-                        class="text-poke-light-blue text-4xl cursor-pointer"
-                        // class={`w-24 cursor-pointer ${
-                        //     !pagination.canNextPage ? 'invisible' : ''
-                        // }`}
-                        onClick={() => setPageNumber(pageNumber() + 1)}
+                        class={`text-poke-light-blue text-4xl w-24 cursor-pointer ${
+                            !canNextPage() ? 'invisible' : ''
+                        }`}
+                        onClick={() => nextPage()}
                     />
                 </div>
             </div>
